43e8e623d9108b7f5c83af5624866d81a465c960
TimyxiaoA <1012177593@qq.com>
2021-02-23 15:57:24 +0800

    禁用eslint

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//! 推荐使用下载的本地字体图标
+import './styles/font/iconfont.css'
+// 加载全局样式
+import './styles/index.less'
+
+// 引入 vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+// 加载 vant
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
